bd53d0b9e45afc2ecf92ea98cab27a8aaa87b57e
alberttgomes <albertgomes1995@hotmail.com>
2022-04-16 23:51:39 -0300

    inialize project SpringBoot

---
+++ placa-mae/src/main/java/com/project/placamae/PlacaMaeApplication.java
@@ -0,0 +1,13 @@
+package com.project.placamae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacaMaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacaMaeApplication.class, args);
+	}
+
+}
---
+++ placa-mae/src/test/java/com/project/placamae/PlacaMaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.placamae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacaMaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
