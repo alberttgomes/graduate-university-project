3d7c8e78680d0543547e4b05d5e4842d2a652e57
alberttgomes <albertgomes1995@hotmail.com>
2022-05-01 20:18:16 -0300

    update projetct front end

--- placa-mae-frontend/src/App.js
+++ placa-mae-frontend/src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
-import NavBar from "./components/navBar";
-
+import "./App.css";
+// import Navbar from "./components/navbar/Navbar";
+// import Header from "./components/header/Header";
+// import Features from "./components/features/Features";
+// import Download from "./components/download/Download";
+// import Subscribe from "./components/subscribe/Subscribe";
+// import Footer from "./components/footer/Footer";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Navbar} from './components'
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App"><NavBar /></div>
+    <>
+       <Navbar />
+       <BrowserRouter>
+           <Routes>
+                <Route  path="/" element={<Home/>}/>
+           </Routes>
+       </BrowserRouter>
+    </>
   );
 }
 
