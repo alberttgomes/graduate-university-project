e114a93ccb14831680a6610c2564d3fab7ca525b
Thiago Oliveira <thiagooliveiraaa@hotmail.com>
2022-06-05 19:37:36 -0300

    create delete and update

--- placa-mae-backend/src/main/java/com/placa/mae/placamae/controller/ControllerPeopleAdolescent.java
+++ placa-mae-backend/src/main/java/com/placa/mae/placamae/controller/ControllerPeopleAdolescent.java
@@ -1,11 +1,15 @@
 package com.placa.mae.placamae.controller;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,20 +28,38 @@ public class ControllerPeopleAdolescent {
 	@Autowired
 	private PeopleAdolescentService peopleAdolescentService;
 	
-	@RequestMapping(value = "/adolescent", method = RequestMethod.GET)
-	public List<PeopleAdolescent> Get(){
+	@RequestMapping(value = "/adolescents", method = RequestMethod.GET)
+	public List<PeopleAdolescent> get(){
 		return daoPeopleAdolescent.findAll();
 	}
 
-	@RequestMapping(value = "adolescent/{id}")
+	@RequestMapping(value = "adolescents/{id}")
 	public ResponseEntity<PeopleAdolescent> findById(@PathVariable Long id){
 		PeopleAdolescent obj = peopleAdolescentService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(value = "/adolescent", method = RequestMethod.POST)
-	public PeopleAdolescent Post(@Validated @RequestBody PeopleAdolescent adolescent) {
+	@RequestMapping(value = "/adolescents", method = RequestMethod.POST)
+	public PeopleAdolescent post(@Validated @RequestBody PeopleAdolescent adolescent) {
 		return daoPeopleAdolescent.save(adolescent);
 	}
 	
+	@DeleteMapping(value="adolescents/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		daoPeopleAdolescent.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value="adolescents/{id}")
+	public ResponseEntity<PeopleAdolescent> update(@PathVariable Long id, @RequestBody PeopleAdolescent obj){
+		Optional<PeopleAdolescent> optionalObj  = daoPeopleAdolescent.findById(id);
+		PeopleAdolescent peopleAdolescent = optionalObj.get();
+		
+		peopleAdolescent.setName(!Objects.isNull(obj.getName()) ? obj.getName() : peopleAdolescent.getName());
+		peopleAdolescent.setAge(!Objects.isNull(obj.getAge()) ? obj.getAge() : peopleAdolescent.getAge());
+		peopleAdolescent.setEmail(!Objects.isNull(obj.getEmail()) ? obj.getEmail() : peopleAdolescent.getEmail());
+		peopleAdolescent.setPassword(!Objects.isNull(obj.getPassword()) ? obj.getPassword() : peopleAdolescent.getPassword());
+		obj = daoPeopleAdolescent.save(peopleAdolescent);
+		return ResponseEntity.ok().body(obj);
+	}
 }
