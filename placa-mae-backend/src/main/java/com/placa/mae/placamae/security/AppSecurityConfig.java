7b0f8952bf3d8c676fbd7d72b58effcb811c2f26
Albert Gomes <albert.gomes@liferay.com>
2022-09-05 18:59:54 -0300

    created class security to adolescents

---
+++ placa-mae-backend/src/main/java/com/placa/mae/placamae/security/AppSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.placa.mae.placamae.security;
+
+import com.placa.mae.placamae.services.PeopleAdolescentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PeopleAdolescentService userDetailsService;
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/adolescents").permitAll()
+                .antMatchers("adolescents/auth").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
