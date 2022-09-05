508149b71d42c79154a31f9fe1f3b2e69d921403
Albert Gomes <albert.gomes@liferay.com>
2022-09-05 18:02:08 -0300

    create class security to class adolescent

--- placa-mae-backend/src/main/java/com/placa/mae/placamae/security/MvcConfig.java
+++ placa-mae-backend/src/main/java/com/placa/mae/placamae/security/MvcConfig.java
@@ -1,6 +1,12 @@
 package com.placa.mae.placamae.security;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class MvcConfig implements WebMvcConfigurer {
+    public void addViewController(@NotNull ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/register").setViewName("register");
+    }
 }
