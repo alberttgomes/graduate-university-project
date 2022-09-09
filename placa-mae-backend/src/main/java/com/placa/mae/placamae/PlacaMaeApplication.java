0cddf5f81af95e02eb275a52d3f0e409758d5044
Albert Gomes <albert.gomes@liferay.com>
2022-09-09 13:07:52 -0300

    sf

--- placa-mae-backend/src/main/java/com/placa/mae/placamae/PlacaMaeApplication.java
+++ placa-mae-backend/src/main/java/com/placa/mae/placamae/PlacaMaeApplication.java
@@ -3,7 +3,6 @@ package com.placa.mae.placamae;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class PlacaMaeApplication {
 	public static void main(String[] args) {
