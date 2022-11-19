e95437de716b3a8090bec916fb3442167bdd6b20
alberttgomes <albertgomes1995@hotmail.com>
2022-11-19 13:20:52 -0300

    Refactory entity kids and material kids

--- src/main/java/com/placa/mae/placamae/domain/MaterialKids.java
+++ src/main/java/com/placa/mae/placamae/domain/MaterialKids.java
@@ -4,10 +4,19 @@ import java.util.Objects;
 
 import javax.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "materialKids", uniqueConstraints = {
 		@UniqueConstraint(columnNames = {"title"})
 })
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class MaterialKids {
 
 		@Id
@@ -15,53 +24,7 @@ public class MaterialKids {
 		private long kidsId;
 		@Column(name = "title", length = 25, nullable = false)
 		private String title;
-		@Column(name = "field", length = 255, nullable = false)
-		private String fieldText;
-		@Column(name = "field_other", length = 255)
-		private String fieldText1;
 
-		public MaterialKids() {
-			super();
-		}
-
-		public MaterialKids(long kidsId, String title, String fieldText, String fieldText1) {
-			this.kidsId = kidsId;
-			this.title = title;
-			this.fieldText = fieldText;
-			this.fieldText1 = fieldText1;
-		}
-
-		public long getKidsId() {
-			return kidsId;
-		}
-
-		public void setKidsId(long kidsId) {
-			this.kidsId = kidsId;
-		}
-
-		public String getTitle(){
-			return title;
-		}
-
-		public void setTitle(String title) {
-			this.title = title;
-		}
-
-		public String getFieldText() {
-			return fieldText;
-		}
-
-		public void setFieldText(String fieldText) {
-			this.fieldText = fieldText;
-		}
-
-		public String getFieldText1() {
-			return fieldText1;
-		}
-
-		public void setFieldText1(String fieldText1) {
-			this.fieldText1 = fieldText1;
-		}
 
 
 		@Override
@@ -71,8 +34,7 @@ public class MaterialKids {
 			}
 			if(obj instanceof MaterialKids) {
 				MaterialKids other = (MaterialKids) obj;
-				return Objects.equals(title, other.title) && Objects.equals(fieldText, other.fieldText)
-								&& Objects.equals(fieldText1, other.fieldText1);
+				return Objects.equals(title, other.title);
 			}
 			return false;
 		}
--- src/main/java/com/placa/mae/placamae/domain/PeopleKids.java
+++ src/main/java/com/placa/mae/placamae/domain/PeopleKids.java
@@ -1,23 +1,33 @@
 package com.placa.mae.placamae.domain;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.*;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "kids", uniqueConstraints = {
 		@UniqueConstraint(columnNames = {"email"}),
 		@UniqueConstraint(columnNames = {"username"})
 })
-public class PeopleKids {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PeopleKids implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long kisId;
-	
-	@Column(name = "name", length = 20, nullable = false)
-	private String name;
 	
 	@Column(name = "age", length = 3, nullable = false)
 	private int age;
@@ -34,71 +44,34 @@ public class PeopleKids {
 	@OneToMany(targetEntity = MaterialKids.class, fetch = FetchType.LAZY)
 	@JoinColumn(name="fk_material_kids")
 	private List<MaterialKids> materialKids = new ArrayList<>();
-
-	public PeopleKids() {
-		super();
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return (Collection<? extends GrantedAuthority>) this.materialKids;
 	}
 
-	public PeopleKids(long kisId, String name, int age, String email, String password, String username) {
-		super();
-		this.age = age;
-		this.email = email;
-		this.kisId = kisId;
-		this.name = name;
-		this.password = password;
-		this.username = username;
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
 	}
 
-	public long getKisId() {
-		return kisId;
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
 	}
 
-	public void setKisId(long kisId) {
-		this.kisId = kisId;
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
 	}
 
-	public String getName() {
-		return name;
-	}
+	@Override
+	public boolean isEnabled() {
+		if(!username.isEmpty()){
+			return true;
+		}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public List<MaterialKids> getMaterialKids() {
-		return materialKids;
+		return false;
 	}
 
 	@Override
@@ -108,10 +81,11 @@ public class PeopleKids {
 		}
 		if(obj instanceof PeopleKids) {
 			PeopleKids other = (PeopleKids) obj;
-			return Objects.equals(name, other.name) && Objects.equals(age, other.age)
-							&& Objects.equals(email, other.email) && Objects.equals(password, other.password)
-							&& Objects.equals(materialKids, other.materialKids);
+			return Objects.equals(age, other.age)
+						&& Objects.equals(email, other.email) && Objects.equals(password, other.password)
+						&& Objects.equals(materialKids, other.materialKids);
 		}
 		return false;
 	}
+
 }
