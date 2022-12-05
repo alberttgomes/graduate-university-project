f480d5e25a13383650457384b49fbd7aa6136185
alberttgomes <albertgomes1995@hotmail.com>
2022-12-05 02:05:08 -0300

    Layer configuration

---
+++ backend/src/main/java/com/placa/mae/org/project/configuration/CorsConfiguration.java
@@ -0,0 +1,26 @@
+package com.placa.mae.org.project.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/adolescents").allowedOrigins("http://localhost:3000").allowedMethods(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"
+        );
+        registry.addMapping("/adults").allowedOrigins("http://localhost:3000").allowedMethods(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"
+        );
+        registry.addMapping("/kids").allowedOrigins("http://localhost:3000").allowedMethods(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"
+        );
+        registry.addMapping("/teachers").allowedOrigins("http://localhost:3000").allowedMethods(
+                "GET", "POST", "PUT", "OPTIONS", "HEAD", "TRACE", "CONNECT"
+        );
+    }
+}
