3f0a875620bc93a93211576d9559179d6f17943a
Albert Gomes <albert.gomes@liferay.com>
2022-12-08 23:23:30 -0300

    Update class Cors Configurations

---
+++ backend/src/main/java/com/project/placa/mae/PlacaMaeOrgApplication.java
@@ -0,0 +1,13 @@
+package com.project.placa.mae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacaMaeOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacaMaeOrgApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/placa/mae/PlacaMaeOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.placa.mae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacaMaeOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
